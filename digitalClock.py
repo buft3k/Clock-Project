f9b6cd282c52dffe697d236ad7cba9f20b95f2b9
buftek21 <88406266+buftek21@users.noreply.github.com>
2023-07-20 00:25:30 -0700

    added button

--- digitalClock.py
+++ digitalClock.py
@@ -7,7 +7,15 @@ def time():
     clock.config(text = myTime)
     clock.after(1000,time)
 clock = tk.Label(myWindow, font = ('arial', 40,'bold'), background = 'dark green', foreground = 'white')
-
 clock.pack(anchor = 'center')
+
+
+myEntry = tk.Entry(myWindow)
+myEntry.pack()
+
+button = tk.Button(myWindow,text = 'To Do List', font = ('Arial', 15))
+button.pack(padx = 10, pady = 10)
+
+
 time()
 myWindow.mainloop()
