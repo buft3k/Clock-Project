6966bee5b0f9b99879a3a28845c777d85fa2978d
buftek21 <88406266+buftek21@users.noreply.github.com>
2023-07-20 00:34:23 -0700

    centered clock & removed buttons

--- digitalClock.py
+++ digitalClock.py
@@ -1,20 +1,22 @@
 import tkinter as tk
 from time import strftime
 myWindow = tk.Tk()
+
+myWindow.geometry("500x500")
 myWindow.title('Time Your Tasks')
 def time():
     myTime = strftime('%I:%M:%S %p')
     clock.config(text = myTime)
     clock.after(1000,time)
 clock = tk.Label(myWindow, font = ('arial', 40,'bold'), background = 'dark green', foreground = 'white')
-clock.pack(anchor = 'center')
+clock.place(x="0",y="0", height= 50, width= 500)
 
 
-myEntry = tk.Entry(myWindow)
-myEntry.pack()
+#myEntry = tk.Entry(myWindow)
+#myEntry.pack()
 
-button = tk.Button(myWindow,text = 'To Do List', font = ('Arial', 15))
-button.pack(padx = 10, pady = 10)
+#button = tk.Button(myWindow,text = 'To Do List', font = ('Arial', 15))
+#button.pack(padx = 10, pady = 10)
 
 
 time()
